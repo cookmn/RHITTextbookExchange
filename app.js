d35c454b92dcf9d3c323b1705165edf6b7bcb020
Katherine Lee <leekf@rose-hulman.edu>
2016-10-30 16:16:38 -0400

    trying to get pages to host on port 3000

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var db = require('./model/db');
+var db = require('./backend/model/db');
 
-// var routes = require('./routes/index');
+ var routes = require('./backend/routes/index');
+ var bookDetails = require('./backend/routes/bookDetail');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
+ app.use('/', routes);
+ app.use('/bookDetails', bookDetails);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
