b99d70beb76d8713ecd4f4ed3480f989f41d28b1
Katherine Lee <leekf@rose-hulman.edu>
2016-10-30 16:39:30 -0400

    trying to get pages to show up on localhost

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var db = require('./backend/model/db');
 
@@ -13,8 +14,9 @@ var db = require('./backend/model/db');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs');
+ //app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
