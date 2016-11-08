89bc9b04548812a81121ddf738b2c3d3d636c979
Katherine Lee <leekf@rose-hulman.edu>
2016-11-08 16:31:36 -0500

    adding auth

--- app.js
+++ app.js
@@ -43,13 +43,6 @@ var rosefire = new RosefireTokenVerifier(SECRET);
  app.use('/sellOrders', sellOrders);
  app.use('/buyOrders', buyOrders);
 
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 // error handlers
 
 //development error handler
